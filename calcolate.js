babf8d03bece1eb20a5f437db5513e66ff0d576e
Kawsar Ahmed <76034051+sami019325@users.noreply.github.com>
2021-08-15 22:47:40 +0600

    final version

--- calcolate.js
+++ calcolate.js
@@ -1,33 +1,26 @@
-// save
-const Car = num1sami = document.getElementById("presvaluesami").innerText;
-  
-  window.localStorage.setItem('car', JSON.stringify(Car));
-
-
-
-//
+/// sami sami sami sami sami sami sami sami sami sami sami sami sami sami sami sami sami
 
 function prevsami(){
     samip=parseInt(num1sami);
     document.getElementById("prevaluesami").innerHTML = samip;
 }
 
-
 function mnssami()
 {
         num1sami = document.getElementById("presvaluesami").innerText;
         num2sami = document.getElementById("rstlsami").value;
-        sami=parseInt(num1sami)-parseInt(num2sami);
-       
         
-        
+        if( num2sami !== null &&  num2sami !== '') {
+            sami=parseInt(num1sami)-parseInt(num2sami); 
+         }
+              
 }
 
 function sumsami (){
     document.getElementById("presvaluesami").innerHTML = sami;
 }
 
-function clrtext(){
+function clrtextsami(){
     document.getElementById("rstlsami").value=null;
 
 }
@@ -39,12 +32,14 @@ function prevsamipl(){
     document.getElementById("prevaluesami").innerHTML = samip;
 }
 
-
 function mnssamipl()
 {
         num1sami = document.getElementById("presvaluesami").innerText;
         num2sami = document.getElementById("rstlsami").value;
-        sami=parseInt(num1sami)+parseInt(num2sami);
+        if( num2sami !== null &&  num2sami !== '') {
+            sami=parseInt(num1sami)+parseInt(num2sami);
+         }
+        
         
 }
 
@@ -52,7 +47,7 @@ function sumsamipl (){
     document.getElementById("presvaluesami").innerHTML = sami;
 }
 
-function clrtext(){
+function clrtextsami2(){
     document.getElementById("rstlsami").value=null;
 
 }
@@ -63,4 +58,348 @@ function resetsami(){
 }
 
 
+/// monika monika monika monika monika monika monika monika monika monika monika monika monika monika monika monika monika
 
+
+function prevmonika(){
+    monikap=parseInt(num1monika);
+    document.getElementById("prevaluemonika").innerHTML = monikap;
+}
+
+function mnsmonika()
+{
+        num1monika = document.getElementById("presvaluemonika").innerText;
+        num2monika = document.getElementById("rstlmonika").value;  
+        if( num2monika !== null &&  num2monika !== '') {
+            monika=parseInt(num1monika)-parseInt(num2monika); 
+         }    
+}
+
+function summonika (){
+    document.getElementById("presvaluemonika").innerHTML = monika;
+}
+
+function clrtextmonika(){
+    document.getElementById("rstlmonika").value=null;
+
+}
+
+// the part of plus
+
+function prevmonikapl(){
+    monikap=parseInt(num1monika);
+    document.getElementById("prevaluemonika").innerHTML = monikap;
+}
+
+function mnsmonikapl()
+{
+        num1monika = document.getElementById("presvaluemonika").innerText;
+        num2monika = document.getElementById("rstlmonika").value;
+        if( num2monika !== null &&  num2monika !== '') {
+            monika=parseInt(num1monika)+parseInt(num2monika); 
+         }
+        
+}
+
+function summonikapl (){
+    document.getElementById("presvaluemonika").innerHTML = monika;
+}
+
+function clrtextmonika2(){
+    document.getElementById("rstlmonika").value=null;
+
+}
+
+// reset the value
+function resetmonika(){
+    document.getElementById("presvaluemonika").innerHTML="500";
+}
+
+/// mahin mahin mahin mahin mahin mahin mahin mahin mahin mahin mahin mahin mahin mahin mahin mahin mahin
+
+
+function prevmahin(){
+    mahinp=parseInt(num1mahin);
+    document.getElementById("prevaluemahin").innerHTML = mahinp;
+}
+
+function mnsmahin()
+{
+        num1mahin = document.getElementById("presvaluemahin").innerText;
+        num2mahin = document.getElementById("rstlmahin").value;
+        if( num2mahin !== null &&  num2mahin !== '') {
+            mahin=parseInt(num1mahin)-parseInt(num2mahin); 
+         }       
+}
+
+function summahin (){
+    document.getElementById("presvaluemahin").innerHTML = mahin;
+}
+
+function clrtextmahin(){
+    document.getElementById("rstlmahin").value=null;
+
+}
+
+// the part of plus
+
+function prevmahinpl(){
+    mahinp=parseInt(num1mahin);
+    document.getElementById("prevaluemahin").innerHTML = mahinp;
+}
+
+function mnsmahinpl()
+{
+        num1mahin = document.getElementById("presvaluemahin").innerText;
+        num2mahin = document.getElementById("rstlmahin").value;
+        if( num2mahin !== null &&  num2mahin !== '') {
+            mahin=parseInt(num1mahin)+parseInt(num2mahin); 
+         }
+        
+}
+
+function summahinpl (){
+    document.getElementById("presvaluemahin").innerHTML = mahin;
+}
+
+function clrtextmahin2(){
+    document.getElementById("rstlmahin").value=null;
+
+}
+
+// reset the value
+function resetmahin(){
+    document.getElementById("presvaluemahin").innerHTML="500";
+}
+
+
+/// joy joy joy joy joy joy joy joy joy joy joy joy joy joy joy joy joy
+
+
+function prevjoy(){
+    joyp=parseInt(num1joy);
+    document.getElementById("prevaluejoy").innerHTML = joyp;
+}
+
+function mnsjoy()
+{
+        num1joy = document.getElementById("presvaluejoy").innerText;
+        num2joy = document.getElementById("rstljoy").value;
+        if( num2joy !== null &&  num2joy !== '') {
+            joy=parseInt(num1joy)-parseInt(num2joy); 
+         }       
+}
+
+function sumjoy (){
+    document.getElementById("presvaluejoy").innerHTML = joy;
+}
+
+function clrtextjoy(){
+    document.getElementById("rstljoy").value=null;
+
+}
+
+// the part of plus
+
+function prevjoypl(){
+    joyp=parseInt(num1joy);
+    document.getElementById("prevaluejoy").innerHTML = joyp;
+}
+
+function mnsjoypl()
+{
+        num1joy = document.getElementById("presvaluejoy").innerText;
+        num2joy = document.getElementById("rstljoy").value;
+        if( num2joy !== null &&  num2joy !== '') {
+            joy=parseInt(num1joy)+parseInt(num2joy); 
+         }
+        
+}
+
+function sumjoypl (){
+    document.getElementById("presvaluejoy").innerHTML = joy;
+}
+
+function clrtextjoy2(){
+    document.getElementById("rstljoy").value=null;
+
+}
+
+// reset the value
+function resetjoy(){
+    document.getElementById("presvaluejoy").innerHTML="500";
+}
+
+
+
+/// jayan jayan jayan jayan jayan jayan jayan jayan jayan jayan jayan jayan jayan jayan jayan jayan jayan
+
+
+function prevjayan(){
+    jayanp=parseInt(num1jayan);
+    document.getElementById("prevaluejayan").innerHTML = jayanp;
+}
+
+function mnsjayan()
+{
+        num1jayan = document.getElementById("presvaluejayan").innerText;
+        num2jayan = document.getElementById("rstljayan").value;
+        if( num2jayan !== null &&  num2jayan !== '') {
+            jayan=parseInt(num1jayan)-parseInt(num2jayan); 
+         }       
+}
+
+function sumjayan (){
+    document.getElementById("presvaluejayan").innerHTML = jayan;
+}
+
+function clrtextjayan(){
+    document.getElementById("rstljayan").value=null;
+
+}
+
+// the part of plus
+
+function prevjayanpl(){
+    jayanp=parseInt(num1jayan);
+    document.getElementById("prevaluejayan").innerHTML = jayanp;
+}
+
+function mnsjayanpl()
+{
+        num1jayan = document.getElementById("presvaluejayan").innerText;
+        num2jayan = document.getElementById("rstljayan").value;
+        if( num2jayan !== null &&  num2jayan !== '') {
+            jayan=parseInt(num1jayan)+parseInt(num2jayan); 
+         }
+        
+}
+
+function sumjayanpl (){
+    document.getElementById("presvaluejayan").innerHTML = jayan;
+}
+
+function clrtextjayan2(){
+    document.getElementById("rstljayan").value=null;
+
+}
+
+// reset the value
+function resetjayan(){
+    document.getElementById("presvaluejayan").innerHTML="500";
+}
+
+/// alvee alvee alvee alvee alvee alvee alvee alvee alvee alvee alvee alvee alvee alvee alvee alvee alvee
+
+
+function prevalvee(){
+    alveep=parseInt(num1alvee);
+    document.getElementById("prevaluealvee").innerHTML = alveep;
+}
+
+function mnsalvee()
+{
+        num1alvee = document.getElementById("presvaluealvee").innerText;
+        num2alvee = document.getElementById("rstlalvee").value;
+        if( num2alvee !== null &&  num2alvee !== '') {
+            alvee=parseInt(num1alvee)-parseInt(num2alvee); 
+         }       
+}
+
+function sumalvee (){
+    document.getElementById("presvaluealvee").innerHTML = alvee;
+}
+
+function clrtextalvee(){
+    document.getElementById("rstlalvee").value=null;
+
+}
+
+// the part of plus
+
+function prevalveepl(){
+    alveep=parseInt(num1alvee);
+    document.getElementById("prevaluealvee").innerHTML = alveep;
+}
+
+function mnsalveepl()
+{
+        num1alvee = document.getElementById("presvaluealvee").innerText;
+        num2alvee = document.getElementById("rstlalvee").value;
+        if( num2alvee !== null &&  num2alvee !== '') {
+            alvee=parseInt(num1alvee)+parseInt(num2alvee); 
+         }
+        
+}
+
+function sumalveepl (){
+    document.getElementById("presvaluealvee").innerHTML = alvee;
+}
+
+function clrtextalvee2(){
+    document.getElementById("rstlalvee").value=null;
+
+}
+
+// reset the value
+function resetalvee(){
+    document.getElementById("presvaluealvee").innerHTML="500";
+}
+
+
+/// arefin arefin arefin arefin arefin arefin arefin arefin arefin arefin arefin arefin arefin arefin arefin arefin arefin
+
+
+function prevarefin(){
+    arefinp=parseInt(num1arefin);
+    document.getElementById("prevaluearefin").innerHTML = arefinp;
+}
+
+function mnsarefin()
+{
+        num1arefin = document.getElementById("presvaluearefin").innerText;
+        num2arefin = document.getElementById("rstlarefin").value;
+        if( num2arefin !== null &&  num2arefin !== '') {
+            arefin=parseInt(num1arefin)-parseInt(num2arefin); 
+         }       
+}
+
+function sumarefin (){
+    document.getElementById("presvaluearefin").innerHTML = arefin;
+}
+
+function clrtextarefin(){
+    document.getElementById("rstlarefin").value=null;
+
+}
+
+// the part of plus
+
+function prevarefinpl(){
+    arefinp=parseInt(num1arefin);
+    document.getElementById("prevaluearefin").innerHTML = arefinp;
+}
+
+function mnsarefinpl()
+{
+        num1arefin = document.getElementById("presvaluearefin").innerText;
+        num2arefin = document.getElementById("rstlarefin").value;
+        if( num2arefin !== null &&  num2arefin !== '') {
+            arefin=parseInt(num1arefin)+parseInt(num2arefin); 
+         }
+        
+}
+
+function sumarefinpl (){
+    document.getElementById("presvaluearefin").innerHTML = arefin;
+}
+
+function clrtextarefin2(){
+    document.getElementById("rstlarefin").value=null;
+
+}
+
+// reset the value
+function resetarefin(){
+    document.getElementById("presvaluearefin").innerHTML="500";
+}
